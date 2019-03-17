a48fab5a99d86fdad54d089de455b258d2fcbfbf
Kevin Ullyott <ullyott.kevin@gmail.com>
2019-03-16 22:10:15 -0700

    Use emotion instead of css modules homesteadPath

--- src/HomesteadPath/HomesteadPath.js
+++ src/HomesteadPath/HomesteadPath.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../../node_modules/bulma/css/bulma.css'
-import styles from './HomesteadPath.module.css'
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
 
 const HomesteadPath = ({
   formSubmit,
@@ -8,13 +9,26 @@ const HomesteadPath = ({
   pathClick,
 }) => (
   <div className="modal is-active has-text-centered">
-    <div className={`modal-background ${styles.modalBackground}`} />
+    <div
+      className="modal-background"
+      css={css`
+        background: #000;
+      `}
+    />
     <div className="modal-content">
 
       <form onSubmit={formSubmit}>
 
         <div className="field">
-          <label className={`label ${styles.customLabel}`}>Set the path to your Homestead folder</label>
+          <label
+            className="label"
+            css={css`
+              color: #fff;
+              font-size: 26px;
+            `}
+          >
+            Set the path to your Homestead folder
+          </label>
           <p>{msg}</p>
           <div className="control">
             <input name="path" className="input is-medium" type="text" placeholder="/user/joeshmoe/websites/" onFocus={pathClick} required />
@@ -23,7 +37,18 @@ const HomesteadPath = ({
 
         <div className="field">
           <div className="control">
-            <button id="create-new-submit" className={`button is-link is-large ${styles.customSubmit}`} type="submit">Set Path</button>
+            <button
+              id="create-new-submit"
+              className="button is-link is-large"
+              type="submit"
+              css={css`
+                margin: 0 auto;
+                display: block;
+                margin-top: 6%;
+              `}
+            >
+              Set Path
+            </button>
           </div>
         </div>
 
