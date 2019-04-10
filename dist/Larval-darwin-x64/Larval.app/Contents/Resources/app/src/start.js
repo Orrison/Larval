b849bce54daa02d01314b20cbc96c0b1b97e13fe
Kevin Ullyott <ullyott.kevin@gmail.com>
2019-04-09 23:04:28 -0700

    v0.2.1

--- dist/Larval-darwin-x64/Larval.app/Contents/Resources/app/src/start.js
+++ dist/Larval-darwin-x64/Larval.app/Contents/Resources/app/src/start.js
@@ -33,7 +33,7 @@ function createWindow() {
       }),
   )
 
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', () => {
     mainWindow = null
