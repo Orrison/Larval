688e2eadb3a33ab3ecd87fd58dd211edccc08e52
Kevin Ullyott <ullyott.kevin@gmail.com>
2019-09-22 16:46:49 -0700

    Use emotion instead of css module for SiteList

--- src/SiteList/SiteList.js
+++ src/SiteList/SiteList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import '../../node_modules/bulma/css/bulma.css'
-import style from './SiteList.module.css'
 import SiteListItem from './SiteListItem'
 import SiteOptions from './SiteOptions'
 /** @jsx jsx */
@@ -22,8 +21,39 @@ class SiteList extends Component {
 
   render() {
     return (
-      <div className={`${style.wrapper}`}>
-        <div className={`${style.list}`}>
+      <div css={css`
+        background-color: #202020;
+        height: 50vh;
+        position: relative;
+        padding: 0;
+        font-size: 20px;
+
+        .list {
+            overflow-y: scroll;
+            height: 40vh;
+            background-color: transparent;
+            margin-bottom: 0;
+
+            &::-webkit-scrollbar {
+              width: 10px;
+            }
+
+            &::-webkit-scrollbar-track {
+              background: #000;
+            }
+
+            &::-webkit-scrollbar-thumb {
+              background: linear-gradient(135deg, #f5b976 21%,#af2518 82%);
+            }
+
+            &::-webkit-scrollbar-thumb:hover {
+              background: linear-gradient(135deg, #f5b976 21%,#af2518 82%);
+            }
+        }
+        
+
+      `}>
+        <div className='list'>
           {this.props.list.map((item, index) => {
             
             let optionsDisplay = 'none'
@@ -50,7 +80,45 @@ class SiteList extends Component {
               </div>
             )})}
         </div>
-        <div className={style.createNew} onClick={this.props.click} onKeyDown={this.props.click} role="menuitem" tabIndex="0">
+        <div onClick={this.props.click} onKeyDown={this.props.click} role="menuitem" tabIndex="0" css={css`
+          position: relative;
+          left: 0;
+          text-align: center;
+          width: 100%;
+          height: 10vh;
+          display: flex;
+          background: #5fc7bd;
+
+          &:hover {
+            cursor: pointer;
+          }
+
+          &:after {
+            content: '';
+            display: block;
+            border-width: 2px;
+            border-style: solid;
+            border-image: linear-gradient(135deg, #f5b976 21%,#af2518 82%);
+            border-image-slice: 1;
+            width: 0;
+            position: absolute;
+            left: -5px;
+            bottom: 0;
+            -webkit-transition: 1s ease-in-out;
+            transition: 1s ease-in-out;
+          }
+
+          &:hover:after { 
+            width: 100%;
+            left: 0;
+          }
+
+          p {
+            align-self: center;
+            margin: 0 auto;
+            font-weight: 800;
+          }
+        `}>
           <p>Create New</p>
         </div>
       </div>
