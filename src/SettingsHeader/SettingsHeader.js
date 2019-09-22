ae0d60fed5c51df4f6b8e13414efa52c2fd67e97
Kevin Ullyott <ullyott.kevin@gmail.com>
2019-09-22 16:26:53 -0700

    Move SettingsHeader css into emotion

--- src/SettingsHeader/SettingsHeader.js
+++ src/SettingsHeader/SettingsHeader.js
@@ -2,14 +2,25 @@ import React from 'react'
 import '../../node_modules/bulma/css/bulma.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCog } from '@fortawesome/free-solid-svg-icons'
-import styles from './SettingsHeader.module.css'
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
 
 const SettingsHeader = ( props ) => {
 
   return (
-    <div className={`${styles.SettingsHeader}`}>
+    <div css={css`
+      height: 15vh;
+      background: #202020;
+      display: flex;
+      text-align: center;
+    `}>
       <h1>Vagrant Controls</h1>
-      <FontAwesomeIcon icon={faCog} size="2x" className={`is-pulled-right ${styles.cog}`} onClick={props.settingsClick} />
+      <FontAwesomeIcon icon={faCog} size="2x" className={`is-pulled-right`} onClick={props.settingsClick} css={css`
+        align-self: center;
+        right: 5%;
+        position: relative;
+        cursor: pointer;
+      `}/>
     </div>
   );
 }
