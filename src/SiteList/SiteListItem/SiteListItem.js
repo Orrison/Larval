e8f8aae8d8721d0169e1de7fb8a19fc4d1bace26
Kevin Ullyott <ullyott.kevin@gmail.com>
2019-09-22 16:49:14 -0700

    Use emotion instead of css module for SiteListItem

--- src/SiteList/SiteListItem/SiteListItem.js
+++ src/SiteList/SiteListItem/SiteListItem.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import '../../../node_modules/bulma/css/bulma.css'
-import style from './SiteListItem.module.css'
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
 
 const SiteListItem = (props) => {
   return (
-    <div className={`${style.SiteListItem}`} onClick={()=>props.click()}>
+    <div onClick={()=>props.click()} css={css`
+      padding: 10px 0;
+      
+      &:hover {
+        color: #fff;
+        cursor: pointer;
+        background: #f5b976;
+        background: -moz-linear-gradient(-45deg, #f5b976 21%, #af2518 82%);
+        background: -webkit-linear-gradient(-45deg, #f5b976 21%,#af2518 82%);
+        background: linear-gradient(135deg, #f5b976 21%,#af2518 82%);
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f5b976', endColorstr='#af2518',GradientType=1 );
+      }
+
+      &:first-of-type {
+        margin-top: 10px;
+      }
+    `}>
       <p>{props.site}</p>
     </div>
   )
